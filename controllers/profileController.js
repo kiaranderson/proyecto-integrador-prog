4a2aca1f5d6a332757c7288eb73b241de0d82b2b
kiaranderson <kikuanderson@gmail.com>
2021-04-13 18:14:43 -0300

    info profile

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,6 +1,12 @@
+let userData = require('../data/userData');
+
 let controller = {
     profile: (req, res) => {
-        res.render('profile', {title: 'profile'})
+        res.render('profile', {username: 'oliandbrotab',
+        productos: '8',
+        seguidores: '569',
+        comentarios: '370'
+    })
     },
 }
 
