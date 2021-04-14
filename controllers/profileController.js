867fdee17c38dd1ace85cd2d9c0bbbbc56998513
kiaranderson <kikuanderson@gmail.com>
2021-04-14 00:16:57 -0300

    cambie edit profile

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -2,11 +2,8 @@ let userData = require('../data/userData');
 
 let controller = {
     profile: (req, res) => {
-        res.render('profile', {username: 'oliandbrotab',
-        productos: '8',
-        seguidores: '569',
-        comentarios: '370'
-    })
+        let user = require('../data/userData')
+        res.render('profile', {user: user})
     },
 }
 
---
+++ data/userData.js
@@ -0,0 +1,9 @@
+let user = {
+    name: 'Olivia',
+    username: 'oliandbrotab',
+    productos: 8,
+    seguidores: 568,
+    comentarios: 284
+}
+
+module.exports = user;
