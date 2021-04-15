0fa52124554d8d1dab86755a7188f7830924004f
kiaranderson <kikuanderson@gmail.com>
2021-04-15 17:50:31 -0300

    novedades hecho

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,7 +1,7 @@
-let novedades = require("../data/productData");
 let controller = {
     index: (req, res) => {
-        res.render('index', {title: 'Inicio', novedades: novedades})
+        let productos = require('../data/productData');
+        res.render('index', {title: 'Inicio', novedades: productos.novedades , comentados: productos.comentados})
     },
 }
 
