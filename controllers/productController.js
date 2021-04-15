f6391e635e4181d8631f8c33b39223ca5271850e
kiaranderson <kikuanderson@gmail.com>
2021-04-15 18:25:12 -0300

    pagina de producto

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,7 @@
 let controller = {
     product: (req, res) => {
-        res.render('product', {title: 'Producto'})
+        let productos = require('../data/productData');
+        res.render('product', {title: 'Producto', name: productos.producto.name, descripcion: productos.producto.descripcion, comentarios: productos.producto.comentarios, usuario: productos.producto.comentarios.username , comentario: productos.producto.comentarios.comentario})
     },
 }
 
--- data/productData.js
+++ data/productData.js
@@ -2,22 +2,22 @@ let novedades =    [
     {
         name: "Esmeralda",
         descripcion: "Ubicado en el barrio de Palermo bla bla bla bla ",
-        comentarios: 1234,
+        comentarios: 764,
     },
     {
         name: "Negroni",
         descripcion: "Ubicado en Pilar bla bla bla bla ",
-        comentarios: 1234,
+        comentarios: 1004,
     },
     {
         name: "Barbas",
         descripcion: "Ubicado en la localidad Pilar bla bla bla bla ",
-        comentarios: 1234,
+        comentarios: 729,
     },
     {
         name: "Pope",
         descripcion: "Ubicado en la localidad de pilar bla bla bla bla ",
-        comentarios: 1,
+        comentarios: 962,
     }
 ] 
 
@@ -25,26 +25,57 @@ let comentados = [
         {
             name: "Antares",
             descripcion: "Ubicado en la localidad de pilar bla bla bla bla ",
-            comentarios: 1234,
+            comentarios: 1346,
         },
         {
             name: "Lowells",
             descripcion: "Ubicado en la localidad de San Isidro bla bla bla bla ",
-            comentarios: 1234,
+            comentarios: 2178,
         },
         {
             name: "El Purgatorio",
             descripcion: "Ubicado en el barrio de palermo bla bla bla bla ",
-            comentarios: 1234,
+            comentarios: 1280,
         },
         {
             name: "La Calle Bar",
             descripcion: "Ubicado en el barrio de Palermo bla bla bla bla ",
-            comentarios: 1234,
+            comentarios: 1927,
         }
 ]
+let producto = {
+    name: 'El Purgatorio',
+    descripcion: 'Ubicado en el barrio de palermo bla bla bla bla ',
+    comentarios: [
+        {
+            username: 'jacintafernandez',
+            comentario: 'Muy divertido, la propuesta que sea un speakeasy le da un toque unico!!'
+        },
+        {
+            username: 'juanacunia',
+            comentario: 'Me gusto el bar, tuve que esperar para entrar.'
+        },
+        {
+            username: 'mariaperez',
+            comentario: 'Excelente bar! Gran ubicacion, genial servicio y buen ambiente. Lo recomiendo.'
+        },
+        {
+            username: 'camilabrosens',
+            comentario: 'Recomendado'
+        },
+        {
+            username: 'jorgeaguero',
+            comentario: 'muy bueno, tardaron un poco pero esta bueno'
+        },
+        {
+            username: 'tomashiguain',
+            comentario: 'excelente servicio, voy a volver'
+        }
+    ]
+}
 
 module.exports = {
     novedades: novedades,
-    comentados: comentados
+    comentados: comentados,
+    producto: producto
 }
