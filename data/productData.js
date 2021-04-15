82cf9201971b552968983d6beee38dd9675550ca
Facundo <Facundo@MacBookeFacundo.fibertel.com.ar>
2021-04-15 17:02:39 -0300

    Definiendo categorias y avanzando en el proyecto

---
+++ data/productData.js
@@ -0,0 +1,43 @@
+let novedades =    [
+{
+    name: "Antares",
+    descripcion: "Ubicado en la localidad de pilar bla bla bla bla ",
+    comentarios: 1234,
+},
+
+{
+    name: "Lowells",
+    descripcion: "Ubicado en la localidad de San Isidro bla bla bla bla ",
+    comentarios: 1234,
+},{
+    name: "El Purgatorio",
+    descripcion: "Ubicado en el barrio de palermo bla bla bla bla ",
+    comentarios: 1234,
+},{
+    name: "La Calle Bar",
+    descripcion: "Ubicado en el barrio de Palermo bla bla bla bla ",
+    comentarios: 1234,
+},{
+    name: "Esmeralda",
+    descripcion: "Ubicado en el barrio de Palermo bla bla bla bla ",
+    comentarios: 1234,
+},{
+    name: "Negroni",
+    descripcion: "Ubicado en Pilar bla bla bla bla ",
+    comentarios: 1234,
+},{
+    name: "Barbas",
+    descripcion: "Ubicado en la localidad Pilar bla bla bla bla ",
+    comentarios: 1234,
+},{
+    name: "Pope",
+    descripcion: "Ubicado en la localidad de pilar bla bla bla bla ",
+    comentarios: 1,
+},
+] 
+module.export = {
+
+    novedades:novedades,
+    
+}
+
