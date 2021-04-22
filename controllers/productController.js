264d4c28d21b9a2f495eb3b306fb8a0423edf4e5
kiaranderson <kikuanderson@gmail.com>
2021-04-22 10:18:12 -0300

    imagenes productos

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,7 +1,7 @@
 let controller = {
     product: (req, res) => {
         let productos = require('../data/productData');
-        res.render('product', {title: 'Producto', name: productos.producto.name, descripcion: productos.producto.descripcion, comentarios: productos.producto.comentarios, usuario: productos.producto.comentarios.username , comentario: productos.producto.comentarios.comentario})
+        res.render('product', {title: 'Producto', name: productos.producto.name, descripcion: productos.producto.descripcion, comentarios: productos.producto.comentarios, usuario: productos.producto.comentarios.username , comentario: productos.producto.comentarios.comentario , img: productos.producto.img})
     },
 
         productAdd: (req, res) => {
