7d0d128b7cda762142b743a07986c888daa8747c
Niky <NikyB@MacBook-Pro-2.fibertel.com.ar>
2021-04-23 12:29:33 -0300

    botones css

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -5,6 +5,6 @@ let controller = require('../controllers/productController');
 
 router.get('/', controller.product); //product/
 
-router.get('/add', controller.productAdd); //product/add
+router.get('/add', controller.productAdd); //product-add
 
 module.exports = router;
