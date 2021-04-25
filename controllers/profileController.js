e1494b9b6d78bea03629b29886b247e8698fd40e
kiaranderson <kikuanderson@gmail.com>
2021-04-25 20:17:13 -0300

    productos profile

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,7 +1,8 @@
 let controller = {
     profile: (req, res) => {
         let user = require('../data/userData')
-        res.render('profile', {user: user, title: 'Perfil'});
+        let productos = require('../data/productData');
+        res.render('profile', {user: user, title: 'Perfil', productos: productos.novedades});
         res.render('headerLogueado', {user: user.name})
     },
 }
