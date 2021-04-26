8043eb0d148f9f1b8f75c87280b6e212c22f6454
kiaranderson <kikuanderson@gmail.com>
2021-04-25 23:37:48 -0300

    ultimos cambios

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 let controller = require('../controllers/productController');
 
-router.get('/', controller.product); //product/
+router.get('/', controller.product);
 
-router.get('/add', controller.productAdd); //product/add
+router.get('/add', controller.productAdd); 
 
 module.exports = router;
