7c4d491b95629b5ceabcda5dec1fa9482909de25
Niky <NikyB@MacBook-Pro-2.fibertel.com.ar>
2021-05-29 11:57:04 -0300

    Edit boton hecho

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -19,3 +19,12 @@ let controller = {
 }
 
 module.exports = controller;
+
+
+// //-cambiar la ruta de profile, el tema de los ID %
+// -login
+// -register
+// -delete
+// -dentro de pro, 1. copiar controlador de product, find all para la lista de los productos
+// -vista de profile information real (find by pk)
+// -revisar las vistas, y cuales necesitan información
