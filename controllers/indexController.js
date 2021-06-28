373b66732ecb74b457153828341a9d44b1bc952f
brooksnicole98 <63105840+brooksnicole98@users.noreply.github.com>
2021-06-28 01:41:53 -0300

    search bar mal

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -45,55 +45,51 @@ let controller = {
    
     searchResults: (req, res) => {
         const filtro = {
-            where: 
-                { //otro operador de sequelize, busca en base a los dos parametros por separado
-                    [Op.or]:[{
-                        product_name: {[Op.like]:'%' + req.query.search + '%'},
-                       
-                    },{
-                        description: {[Op.like]:'%' + req.query.search + '%'}
-                    }]
-                }
+            where: { //otro operador de sequelize, busca en base a los dos parametros por separado
+                [Op.or]: [{
+                    product_name: {
+                        [Op.like]: '%' + req.query.search + '%'
+                    },
+
+                }, {
+                    description: {
+                        [Op.like]: '%' + req.query.search + '%'
+                    }
+                }]
+            }
 
         }
         db.Product.findAll(filtro)
-        .then(result => {
-           
-            res.render('search-results',{
-                productos: result
-            })
-        })
-      },
+            .then(result => {
+                    if (result == '' || req.query.search == "") {
+                        console.log('no se encuentran resultados');
+                        console.log(JSON.stringify(resultado))
 
-           
+                        res.render('search-results', {
+                            resultado: result,
+                            error: 'No existen resultados',
+                        });
+                    } else {
+                        console.log(JSON.stringify(resultado))
+                        console.log('Si hay resultados');
+                        res.render('search-results', {
+                            resultado: result,
+                            erros: null
+                        })
+                    }
+
+            })
+            }
+
+
     }
-    db.Product.findAll(filtro)
-    .then(result => {
-        if(result == ''|| req.query.search == "") {
-            console.log('no se encuentran resultados');
-            console.log(JSON.stringify(resultado))
-
-            res.render('search-results',{
-                resultado: resultado,
-                error: 'No existen resultados',
-            });
-        } else {
-            console.log(JSON.stringify(resultado))
-            console.log('Si hay resultados');
-            res.render('search-results',{
-                resultado:resultado,
-                erros: null
-            })
-        }
-       
-        res.render('search-results',{
-            productos: result
-        })
-    })
 
 
 
-  
+
+
+
+    
 
 
 module.exports = controller;
